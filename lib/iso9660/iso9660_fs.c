a29b830ddd48602b6308f230aeae8773cdf5e531
Kevin Scroggins <nitro404@gmail.com>
2026-02-14 14:00:24 -0500

    Fix MSVC undefined symbol linker error

--- lib/iso9660/iso9660_fs.c
+++ lib/iso9660/iso9660_fs.c
@@ -1815,6 +1815,12 @@ iso9660_fs_find_lsn(CdIo_t *p_cdio, lsn_t i_lsn)
 iso9660_stat_t *
 #if defined(__GNUC__) && !defined(__DARWIN_C_ANSI)
 iso9660_find_fs_lsn(CdIo_t *p_cdio, lsn_t i_lsn) __attribute__ ((alias ("iso9660_fs_find_lsn")));
+#elif defined(_MSC_VER)
+iso9660_stat_t *
+iso9660_find_fs_lsn(CdIo_t *p_cdio, lsn_t i_lsn)
+{
+    return iso9660_fs_find_lsn(p_cdio, i_lsn);
+}
 #else
 iso9660_find_fs_lsn(CdIo_t *p_cdio, lsn_t i_lsn);
 #endif
