768c52b0d648b622f0b575f9b30abe002e163e96
Kevin Scroggins <nitro404@gmail.com>
2026-02-14 14:05:51 -0500

    Fix typo

--- lib/iso9660/iso9660_fs.c
+++ lib/iso9660/iso9660_fs.c
@@ -1816,7 +1816,6 @@ iso9660_stat_t *
 #if defined(__GNUC__) && !defined(__DARWIN_C_ANSI)
 iso9660_find_fs_lsn(CdIo_t *p_cdio, lsn_t i_lsn) __attribute__ ((alias ("iso9660_fs_find_lsn")));
 #elif defined(_MSC_VER)
-iso9660_stat_t *
 iso9660_find_fs_lsn(CdIo_t *p_cdio, lsn_t i_lsn)
 {
     return iso9660_fs_find_lsn(p_cdio, i_lsn);
